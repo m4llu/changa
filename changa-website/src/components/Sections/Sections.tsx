8f70eade805b6f86e165ef304c63afe2bcf39881
m4llu <hynninenallu07@gmail.com>
2024-09-23 12:04:59 +0300

    ju

--- changa-website/src/components/Sections/Sections.tsx
+++ changa-website/src/components/Sections/Sections.tsx
@@ -4,8 +4,7 @@ const Sections: React.FC = () => {
   return (
     <>
       <section>
-        <h1>Get ready to edge with Kai Cenat!</h1>
-        <p>This is the home section.</p>
+
       </section>
     </>
   );
